ca9f45a53071529f5f5ba6b694175a2d12a6df75
KarineFrontelli <you@example.comkarinefrontellidossantos@gmail.com>
2023-03-23 15:05:36 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from "react-icons/fi";
+
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <h1 className="App-title">Buscador de CEP</h1>
+
+      <div className="App-container-input">
+        <input type="text" placeholder="Digite seu CEP" />
+        <button className="App-button">
+          <FiSearch size={25} color="#fff" />
+        </button>
+      </div>
+
+      <main className="App-main">
+        <h2>CEP:</h2>
+
+        <span>Rua:</span>
+        <span>Complemento:</span>
+        <span>Bairro:</span>
+        <span>Cidade:</span>
+      </main>
     </div>
   );
 }
