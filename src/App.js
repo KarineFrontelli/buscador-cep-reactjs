a5dcd2ed8f54b2c26fc31db30d88b577a7c07853
KarineFrontelli <you@example.comkarinefrontellidossantos@gmail.com>
2023-03-23 15:50:17 -0300

    Instalado axios para consumo de API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import api from "./services/api";
 
 import "./style.css";
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+ async function handleSearch() {
+  if (input === '') {
+    alert('Digite o CEP')
+    return
+  }
+  try {
+    const response = await api.get(`${input}/json`);
+    console.log(response.data)
+  } catch (error) {
+    alert('Erro ao buscar')
+    setInput('')
+  }
+ }
+
   return (
     <div className="App-container">
       <h1 className="App-title">Buscador de CEP</h1>
 
       <div className="App-container-input">
-        <input type="text" placeholder="Digite seu CEP" />
-        <button className="App-button">
+        <input type="text" placeholder="Digite seu CEP" value={input} onChange={(e) => setInput(e.target.value)} />
+
+        <button className="App-button" onClick={handleSearch}>
           <FiSearch size={25} color="#fff" />
         </button>
       </div>
